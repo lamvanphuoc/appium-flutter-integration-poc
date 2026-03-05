0c51c1ed877e51dbe7db932900597ac717eabb96
lam van <lam.van@cartoncloud.com>
2026-03-05 08:51:58 +0700

    add iOS app

---
+++ ios-test-app/ios-test-app/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ios-test-app
+//
+//  Created by Lam Van on 5/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button("Open Flutter app") {
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-test-app/ios-test-app/ios_test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_test_appApp.swift
+//  ios-test-app
+//
+//  Created by Lam Van on 5/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct ios_test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
