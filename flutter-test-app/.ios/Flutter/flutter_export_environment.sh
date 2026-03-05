e70760aa72497d5fe3e30717e141b93718f8a240
lam van <lam.van@cartoncloud.com>
2026-03-05 11:25:48 +0700

    update iOS app with flutter module

--- flutter-test-app/.ios/Flutter/flutter_export_environment.sh
+++ flutter-test-app/.ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/lamvan/fvm/versions/3.32.5"
-export "FLUTTER_APPLICATION_PATH=/Users/lamvan/Projects/mobile-cartoncloud/appium test poc/integration_test_sample"
+export "FLUTTER_APPLICATION_PATH=/Users/lamvan/Projects/mobile-cartoncloud/appium-flutter-integration-poc/flutter-test-app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/lamvan/Projects/mobile-cartoncloud/appium-flutter-integration-poc/flutter-test-app/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9WRVJTSU9OPTMuMzIuNQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZmNmMmMxMTU3Mg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049ZGQ5M2RlNmZiMQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My44LjE="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/lamvan/Projects/mobile-cartoncloud/appium-flutter-integration-poc/flutter-test-app/.dart_tool/package_config.json"
---
+++ flutter-test-app/.ios/Pods/Target Support Files/a_bridge/a_bridge-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_a_bridge : NSObject
+@end
+@implementation PodsDummy_a_bridge
+@end
